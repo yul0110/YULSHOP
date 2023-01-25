43b60c52e94187ecaace34dd0aa59dfeca4603c2
Lee Yul <gpal0110naver.com>
2023-01-25 23:03:54 +0900

    파일업로드 수정 폴더 생성 로직에서 파일명+yyyymmddhhmmss+확장자로 중복 오류 처리

--- workspace/yulShop/src/main/java/com/controller/ImgController.java
+++ workspace/yulShop/src/main/java/com/controller/ImgController.java
@@ -29,7 +29,7 @@ public class ImgController {
 		return "imgUpload/imgUpload";
     }
 	
-	//이미지 단수 전용 파일 처리 ajax
+	//이미지 단수 전용 파일 처리 ajax - 업로드마다 yyyymmddhhmmss 기준으로 폴더를 생성
 	@PostMapping("/uploadImgAjax")
     public ModelAndView uploadFile(@RequestParam MultipartFile uploadFile) throws IOException{ //파일업로드인경우 파일관련 IO익셉션을 사용해야한다.	
 		
@@ -40,18 +40,20 @@ public class ImgController {
     	//2. 현재 로컬시간 = 현재 컴퓨터의 시간을 가져와서 .now() --> 현재시간을 지정	
     	String todayPath 		= LocalDateTime.now().format(dtf);	// String으로 되어 있는 날짜와 시간을 포맷에 맞게 파싱
     	//3. 저장할 경로를 hard coding으로 작성
-    	String basePath 		= "C:\\java_dev\\2.project\\yulShop\\workspace\\yulShop\\workspace\\yulShop\\src\\main\\resources\\loadImg";
+    	String basePath 		= "C:\\java_dev\\2.project\\yulShop\\workspace\\yulShop\\workspace\\yulShop\\src\\main\\resources\\loadImg\\imgYulshop";
     	//4. 화면에 출력할 경로에 맞게 처리 --> webConfig에서 실제 파일경로 핸들러 작성해야됨
-    	String ResourcesPath	= "/loadImg/"+ todayPath + "/" + uploadFile.getOriginalFilename();
     	
     	//5. 경로 객체에 두가지 데이터 합침 -- 합친 이유는 파일이 중복되지 않게 처리하기 위해
-    	Path pathToday = Paths.get(basePath, todayPath);	// BasePath 와 현재 날짜를 합친 Path 객체 생성
-    	//파일을 넣으려는데 폴더가 없는 경우 알아서 생성하는 코드
-        if (Files.notExists(pathToday)) {		//저장 할 경로에 폴더가 없을 경우 폴더 생성		  
-            Files.createDirectories(pathToday);
-        }
+		Path pathToday 		= Paths.get(basePath, todayPath);	// BasePath 와 현재 날짜를 합친 Path 객체 생성
+		String fullName 	= uploadFile.getOriginalFilename();	//파일명
+		String fileName 	= fullName.substring(0,fullName.lastIndexOf(".")); //파일명의 0번째부터 "."전까지 잘라 파일명만 가져온다
+		String ext		 	= fullName.substring(fullName.lastIndexOf(".")); //파일명에서 "." 포함 나머지 문자열 확장자를 가져온다
         
-        Path targetPath = Paths.get(pathToday.toString(), uploadFile.getOriginalFilename()); //BasePath + 날짜 + 파일명을 합친 Path 객체 생성
+		String pathName = fileName + todayPath + ext;
+		
+		String ResourcesPath	= "/loadImg/imgYulshop/" + pathName;
+		
+        Path targetPath = Paths.get(basePath, pathName); //BasePath + 날짜 + 파일명을 합친 Path 객체 생성
         //6. 받아온 파일을 내가 작성한 하드코딩 경로 3번에 파일을 복사 처리 
         uploadFile.transferTo(targetPath); 
         //file 을 outputStream 으로 내보내 저장.
@@ -59,29 +61,37 @@ public class ImgController {
     	mv.addObject("path",ResourcesPath);
     	return mv;
 	}
+	
+	
+	//이미지 단수 전용 파일 처리 ajax - 업로드마다 yyyymmddhhmmss 기준으로 폴더를 생성
+	@PostMapping("/uploadImgAjax2")
+	public ModelAndView uploadFile2(@RequestParam MultipartFile uploadFile) throws IOException{ //파일업로드인경우 파일관련 IO익셉션을 사용해야한다.	
 		
+		ModelAndView mv = new ModelAndView("jsonView");
+		
+		//1. date 포멧 설정 객체로 원하는 폼타입 결정
+		DateTimeFormatter dtf 	= DateTimeFormatter.ofPattern("yyyymmddhhmmss");	
+		//2. 현재 로컬시간 = 현재 컴퓨터의 시간을 가져와서 .now() --> 현재시간을 지정	
+		String todayPath 		= LocalDateTime.now().format(dtf);	// String으로 되어 있는 날짜와 시간을 포맷에 맞게 파싱
+		//3. 저장할 경로를 hard coding으로 작성
+		String basePath 		= "C:\\java_dev\\2.project\\yulShop\\workspace\\yulShop\\workspace\\yulShop\\src\\main\\resources\\loadImg";
+		//4. 화면에 출력할 경로에 맞게 처리 --> webConfig에서 실제 파일경로 핸들러 작성해야됨
+		String ResourcesPath	= "/loadImg/"+ todayPath + "/" + uploadFile.getOriginalFilename();
+		
+		//5. 경로 객체에 두가지 데이터 합침 -- 합친 이유는 파일이 중복되지 않게 처리하기 위해
+		Path pathToday = Paths.get(basePath, todayPath);	// BasePath 와 현재 날짜를 합친 Path 객체 생성
+		//파일을 넣으려는데 폴더가 없는 경우 알아서 생성하는 코드
+		if (Files.notExists(pathToday)) {		//저장 할 경로에 폴더가 없을 경우 폴더 생성		  
+			Files.createDirectories(pathToday);
+		}
+		
+		Path targetPath = Paths.get(pathToday.toString(), uploadFile.getOriginalFilename()); //BasePath + 날짜 + 파일명을 합친 Path 객체 생성
+		//6. 받아온 파일을 내가 작성한 하드코딩 경로 3번에 파일을 복사 처리 
+		uploadFile.transferTo(targetPath); 
+		//file 을 outputStream 으로 내보내 저장.
+		
+		mv.addObject("path",ResourcesPath);
+		return mv;
+	}
+	
 }
-		
-		
-//		String uploadFolder = "C:\\java_dev\\2.project\\ImgUpload";
-//		
-//		for(MultipartFile multipartFile : uploadFile) {
-//			
-//			System.out.println("업로드 파일 이름:"+multipartFile.getOriginalFilename());
-//			System.out.println("업로드 파일 크기:"+multipartFile.getSize());
-//			
-//			String uploadFileName = multipartFile.getOriginalFilename();
-//			
-//			//IE has file path
-//			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
-//			System.out.println("only file name : " + uploadFileName);
-//			
-//			File saveFile = new File(uploadFolder);
-//			
-//				try {
-//					multipartFile.transferTo(saveFile);
-//				}catch (Exception e) {
-//					System.out.println(e.getMessage());
-//				}//end catch
-//		}//end for
-
