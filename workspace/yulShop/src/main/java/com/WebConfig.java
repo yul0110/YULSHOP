53e9c9e9dfe2f4e19e27b53bbcc39fd68deb20e9
snj <kswoah123@naver.com>
2023-02-18 02:38:04 +0900

    시큐리티 임시 해제

--- workspace/yulShop/src/main/java/com/WebConfig.java
+++ workspace/yulShop/src/main/java/com/WebConfig.java
@@ -9,11 +9,13 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
     
+	// 제이슨 데이터 처리
 	@Bean
 	MappingJackson2JsonView jsonView(){
 		return new MappingJackson2JsonView();
 	}
 	
+	// 이미지를 보여주기 위해 해당 경로를 열어줌
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/loadImg/**").addResourceLocations("file:///C:/java_dev/2.project/yulShop/workspace/yulShop/workspace/yulShop/src/main/resources/loadImg/");
